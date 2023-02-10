c42d680840c93b75642028aae9d7f4f6b4b8b5cd
liu <1573619809@qq.com>
2023-02-10 14:40:13 +0800

    使用轻量级时间转换替换moment

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,9 +1,16 @@
 /*
 自定义过滤器模块
  */
-import moment from 'moment'
-import Vue from 'vue'
+// import moment from 'moment'
+// import Vue from 'vue'
 
-Vue.filter('dateString', function (value, format) {
-  return moment(value).format(format || 'YYYY-MM-DD HH:mm:ss')
+// Vue.filter('dateString', function (value, format) {
+//   return moment(value).format(format || 'YYYY-MM-DD HH:mm:ss')
+// })
+
+
+import format from 'date-fns/format'
+import Vue from 'vue'
+Vue.filter('dateString', function(value, formatStr){
+    return format(value, formatStr || 'yyyy-MM-dd HH:mm:ss')
 })
